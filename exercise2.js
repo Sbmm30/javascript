0d6abadad9f7a06a8d851501669f4b8b891aa5aa
sbmm30 <mori.sebastian@outlook.com>
2023-01-26 19:16:36 -0500

    exercise 2

---
+++ exercise2.js
@@ -0,0 +1,14 @@
+const new_list = [
+    "Sbmm30",
+    21,
+    true,
+    new Date('November 30, 2001'),
+    book = {
+        title: "El instituto",
+        author: "Stephen King",
+        date: new Date('September 10, 2019'),
+        url: "https://books.google.com.pe/books/about/El_Instituto.html?id=ha6hDwAAQBAJ&printsec=frontcover&source=kp_read_button&hl=es-419&redir_esc=y#v=onepage&q&f=false",
+    }
+];
+
+console.log(new_list);
